31f024a03015d5022f5afd68e644bd547c8fd4c6
DELL <varkalapawakumar@gmail.com>
2024-10-08 17:47:17 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab006.java
@@ -0,0 +1,13 @@
+package sept.ex_06092024;
+
+public class Lab006 {
+    public static void main(String[] pavan) {
+       /* System.out.println("Hi Pavan");
+        System.out.println("Hi Pavan");
+        System.out.println("Hi Pavan");
+        System.out.println("Hi Pavan"); */
+
+        //Hello this a program
+    }
+
+}
---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,22 @@
+package sept.ex_06092024;
+/*
+Author:pawan
+*
+Can write anything here
+ */
+
+//Program java with class file, name of the class is Lab007
+public class Lab007 {
+
+    // This is the main method, JVM starts from here
+    public static void main(String[] args) {
+        //Below code will be executed and it will print Hello world
+        System.out.println("Hello World");
+
+
+
+
+    }
+
+
+}
